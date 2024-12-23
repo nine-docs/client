1d66bb3433b3dcd6487281d755ee2d35361d4d67
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-12-23 10:43:18 +0900

    Feat. Toast 컴포넌트 CSS 덮어씌움

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import MainPage from "pages/main_page/MainPage";
+import MyPage from "pages/my_page/MyPage";
 import SignInPage from "pages/sign_in_page/SignInPage";
 import SignUpPage from "pages/sign_up_page/SignUpPage";
 
@@ -11,6 +12,7 @@ const Router = () => {
       <Route path="/main" element={<MainPage />} />
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 };
--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { ToastContainer, Zoom } from "react-toastify";
 
+import "./ToastifyCustom.scss";
+
 const Toast = () => {
   return (
     <ToastContainer
       position="bottom-center"
       autoClose={3000}
       transition={Zoom}
+      hideProgressBar={true}
       closeOnClick
       rtl={false}
       pauseOnFocusLoss={false}
