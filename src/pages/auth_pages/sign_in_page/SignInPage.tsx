ca0124260c0253785033177d7ac2b56f4f3316c5
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-12-27 02:13:39 +0900

    Feat. SignInPage onSubmit 핸들러 추가

--- src/pages/auth_pages/sign_in_page/SignInPage.tsx
+++ src/pages/auth_pages/sign_in_page/SignInPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 
 import LogoIcon from "assets/images/logos/LogoIcon";
 
@@ -7,6 +7,13 @@ import TextButton from "components/buttons/text_button/TextButton";
 import BaseInput from "components/inputs/base_input/BaseInput";
 
 import classes from "./SignInPage.module.scss";
+
+type FormValues = {
+  nickname: string;
+  email: string;
+  password: string;
+  passwordCheck: string;
+};
 
 const SignInPage = () => {
   const initialFormValue = {
@@ -21,6 +28,10 @@ const SignInPage = () => {
     defaultValues: initialFormValue,
   });
 
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
+
   return (
     <main className={classes.page_wrap}>
       <FormProvider {...methods}>
@@ -29,7 +40,10 @@ const SignInPage = () => {
             <LogoIcon />
             <h2 className={classes.signin_title}>회원가입</h2>
           </div>
-          <form className={classes.sign_form}>
+          <form
+            className={classes.sign_form}
+            onSubmit={methods.handleSubmit(onSubmit)}
+          >
             <div className={classes.signin_input_wrap}>
               <BaseInput
                 type="text"
@@ -66,10 +80,16 @@ const SignInPage = () => {
                 registerOption={{}}
               />
             </div>
-            <TextButton text="회원가입" width={"100%"} size="xlarge" />
+            <TextButton
+              type="submit"
+              text="회원가입"
+              width={"100%"}
+              size="xlarge"
+            />
             <div className={classes.login_notice_wrap}>
               <span>이미 회원이신가요?</span>
               <TextButton
+                type="button"
                 text="로그인"
                 theme="primary-line"
                 p="s"
