4737aa629e3df899183c67934a5e7a9f3b49fc58
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-12-31 14:55:45 +0900

    Feat. QuestionPage에 react-markdown 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import LoginPage from "pages/auth_pages/login_page/LoginPage";
 import SignUpPage from "pages/auth_pages/sign_up_page/SignUpPage";
 import MainPage from "pages/main_page/MainPage";
 import MyPage from "pages/my_page/MyPage";
+import QuestionPage from "pages/question_page/QuestionPage";
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/mypage/*" element={<MyPage />} />
+      <Route path="/question" element={<QuestionPage />} />
     </Routes>
   );
 };
--- src/pages/question_page/QuestionPage.tsx
+++ src/pages/question_page/QuestionPage.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Markdown from "react-markdown";
+
+import classes from "./QuestionPage.module.scss";
 
 const QuestionPage = () => {
-  return <div></div>;
+  const mdFile = `
+  A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+### sdf
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |
+`;
+
+  return (
+    <div className={classes.page_wrap}>
+      <Markdown>{mdFile}</Markdown>
+    </div>
+  );
 };
 
 export default QuestionPage;
