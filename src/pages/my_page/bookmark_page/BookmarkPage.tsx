a175cae3ba1d0ac8442bbad50d289154e19dd528
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-01-01 14:40:30 +0900

    Feat. 북마크 아이템 onClick 메서드 구현

--- src/pages/my_page/bookmark_page/BookmarkPage.tsx
+++ src/pages/my_page/bookmark_page/BookmarkPage.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import { useGetBookmarkList } from "apis/mypage_apis/useBookmark";
 
 import classes from "./BookmarkPage.module.scss";
 
 const BookmarkPage = () => {
+  const navigate = useNavigate();
+
   const { data } = useGetBookmarkList(1, 10);
 
-  const handleItemClick = () => {};
+  const handleItemClick = (articleId: number) => {
+    navigate(`/article/${articleId}`);
+  };
 
   return (
     <main className={classes.content_wrap}>
@@ -18,7 +23,9 @@ const BookmarkPage = () => {
           <button
             key={item.bookmarkId}
             className={classes.item_wrap}
-            onClick={handleItemClick}
+            onClick={() => {
+              handleItemClick(item.article.id);
+            }}
           >
             <span className={classes.category}>
               {item.article.category.name}
