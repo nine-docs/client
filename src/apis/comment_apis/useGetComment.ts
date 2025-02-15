a4f27ff7abcf5d1fa8ea14e3222d44edc3fe39ce
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-02-15 17:15:56 +0900

    Feat. 댓글 API도 mock으로 테스트 가능하도록 추가

--- src/apis/comment_apis/useGetComment.ts
+++ src/apis/comment_apis/useGetComment.ts
@@ -5,7 +5,67 @@ import queryKeyFactory from "apis/query_config/queryKeyFactory";
 
 const LIMIT = 5;
 
+type CommentType = {
+  commentId: number;
+  author: {
+    id: number;
+    nickname: string;
+    isMe: boolean;
+  };
+  reply: {
+    count: number;
+  };
+  content: string;
+  like: {
+    count: number;
+    isUserLike: boolean;
+  };
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+};
+
+type GetCommentResType = {
+  success: boolean;
+  errorCode?: string;
+  data: {
+    cursor: number | null;
+    items: CommentType[];
+  };
+};
+
+const mockData: GetCommentResType = {
+  success: true,
+  errorCode: "",
+  data: {
+    cursor: null,
+    items: [
+      {
+        commentId: 0,
+        author: {
+          id: 0,
+          nickname: "홍길동",
+          isMe: true,
+        },
+        reply: {
+          count: 0,
+        },
+        content: "댓글 내용입니다.",
+        like: {
+          count: 0,
+          isUserLike: true,
+        },
+        createdAt: "2025-02-15T08:02:49.155Z",
+        updatedAt: "2025-02-15T08:02:49.155Z",
+        deletedAt: "2025-02-15T08:02:49.155Z",
+      },
+    ],
+  },
+};
+
 export const useGetComment = (articleId: number) => {
+  const isApiMock = process.env.REACT_APP_API_MOCK === "true";
+
   const initialUrl: string = `/api/v1/article/${articleId}/comments?limit=${LIMIT}`;
 
   const {
@@ -19,7 +79,15 @@ export const useGetComment = (articleId: number) => {
     data,
   } = useInfiniteQuery({
     queryKey: queryKeyFactory.comment({ articleId: articleId }).queryKey,
-    queryFn: ({ pageParam }) => httpClient.get(pageParam),
+    queryFn: ({ pageParam }): Promise<GetCommentResType> => {
+      if (isApiMock) {
+        return new Promise((resolve) =>
+          setTimeout(() => resolve(mockData), 100),
+        );
+      } else {
+        return httpClient.get(pageParam);
+      }
+    },
     initialPageParam: initialUrl,
     getNextPageParam: (lastPage) => {
       if (!lastPage.data.cursor && typeof lastPage.data.cursor !== "number") {
