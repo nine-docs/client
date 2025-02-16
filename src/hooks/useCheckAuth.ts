bb89d34c5ceec8224a6a2e7544a9ea29b9a215dd
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-02-16 22:20:04 +0900

    Feat. 문제상세 화면은 로그인 안해도 진입 가능하도록 수정

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -27,14 +27,24 @@ const useCheckAuth = () => {
     if (token === "" || accessTokenExpiredAt === "") {
       /* 1. 토큰이 없거나 만료날짜가 없음 */
       deleteAuthInfo(); // 로그아웃
-      // 현재 페이지가 로그인, 회원가입, 메인화면이 아니면 메인화면으로 강제소환
-      if (pathname !== "/login" && pathname !== "/signup" && pathname !== "/") {
+      // 현재 페이지가 로그인, 회원가입, 메인화면, 문제페이지가 아니면 메인화면으로 강제소환
+      if (
+        pathname !== "/login" &&
+        pathname !== "/signup" &&
+        !pathname.includes("article") &&
+        pathname !== "/"
+      ) {
         navigate("/");
       }
     } else if (dayjs(accessTokenExpiredAt).isBefore(dayjs())) {
       /* 2. 토큰 만료 날짜가 지났음 */
       deleteAuthInfo();
-      if (pathname !== "/login" && pathname !== "/signup" && pathname !== "/") {
+      if (
+        pathname !== "/login" &&
+        pathname !== "/signup" &&
+        pathname !== "/" &&
+        !pathname.includes("article")
+      ) {
         navigate("/");
       }
     } else {
