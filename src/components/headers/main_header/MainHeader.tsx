037d5e07e36e4c1e59d6bbf0851c620ccf26d047
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-02-17 21:54:07 +0900

    Feat. 메인헤더 로그인 버튼 조건 수정

--- src/components/headers/main_header/MainHeader.tsx
+++ src/components/headers/main_header/MainHeader.tsx
@@ -7,12 +7,15 @@ import Logo from "assets/images/logos/LogoIcon";
 import BaseButton from "components/buttons/base_button/BaseButton";
 import TextButton from "components/buttons/text_button/TextButton";
 
+import useIsLogin from "hooks/useIsLogin";
+
 import classes from "./MainHeader.module.scss";
 
 const MainHeader = () => {
   const navigate = useNavigate();
 
   const { token, deleteAuthInfo } = useAuthStore();
+  const { isLogin } = useIsLogin();
 
   const handleLogoClick = () => {
     if (token === "") {
@@ -20,6 +23,10 @@ const MainHeader = () => {
     } else {
       navigate("/mypage/subscribe");
     }
+  };
+
+  const handleLoginClick = () => {
+    navigate(`/login`);
   };
 
   const handleLogoutClick = () => {
@@ -37,8 +44,17 @@ const MainHeader = () => {
           <span>구DOCs</span>
         </div>
       </BaseButton>
+      {/* 로그인이 안되어 있는 경우 : 로그인 버튼 */}
+      {!isLogin && (
+        <TextButton
+          text="로그인"
+          p="s"
+          size="small"
+          onClick={handleLoginClick}
+        />
+      )}
       {/* 로그인 되어 있는 경우 : 로그아웃 버튼 */}
-      {token !== "" && (
+      {isLogin && (
         <TextButton
           text="로그아웃"
           p="s"
