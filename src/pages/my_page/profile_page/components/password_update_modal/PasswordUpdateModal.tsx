77eb89e5fea2d072547f6e33ab977c62c7669c19
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-02-23 22:52:08 +0900

    Feat. 비밀번호 변경 UI 추가

--- src/pages/my_page/profile_page/components/password_update_modal/PasswordUpdateModal.tsx
+++ src/pages/my_page/profile_page/components/password_update_modal/PasswordUpdateModal.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { useFormContext } from "react-hook-form";
 
+import TextButton from "components/buttons/text_button/TextButton";
 import BaseInput from "components/inputs/base_input/BaseInput";
 
 import classes from "./PasswordUpdateModal.module.scss";
@@ -30,25 +31,29 @@ const PasswordUpdateModal = ({ onClose }: { onClose: () => void }) => {
         className={classes.modal_content}
         onMouseDown={(e) => e.stopPropagation()}
       >
-        <h3>비밀번호 변경</h3>
-        <label>
-          <div>기존 비밀번호</div>
-          <BaseInput
-            type="password"
-            registerName="password"
-            placeholder="기존 비밀번호"
-            registerOption={{ required: "기존 비밀번호를 입력해 주세요." }}
-          />
-        </label>
-        <label>
-          <div>새 비밀번호</div>
-          <BaseInput
-            type="password"
-            registerName="newPassword"
-            placeholder="새 비밀번호"
-            registerOption={{ required: "기존 비밀번호를 입력해 주세요." }}
-          />
-        </label>
+        <div className={classes.modal_body}>
+          <label className={classes.input_wrap}>
+            <div>기존 비밀번호</div>
+            <BaseInput
+              type="password"
+              registerName="password"
+              placeholder="기존 비밀번호"
+              registerOption={{ required: "기존 비밀번호를 입력해 주세요." }}
+            />
+          </label>
+          <label className={classes.input_wrap}>
+            <div>새 비밀번호</div>
+            <BaseInput
+              type="password"
+              registerName="newPassword"
+              placeholder="새 비밀번호"
+              registerOption={{ required: "기존 비밀번호를 입력해 주세요." }}
+            />
+          </label>
+        </div>
+        <div className={classes.modal_footer}>
+          <TextButton text="변경하기" />
+        </div>
       </motion.div>
     </motion.div>
   );
