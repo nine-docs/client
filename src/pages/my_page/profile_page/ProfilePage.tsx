1c9769cff28fac84a67bbd7bba69fe0b700e0ee5
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-02-24 21:25:09 +0900

    Feat. 비밀번호 변경과 이메일/닉네임의 FormProvider를 분리

--- src/pages/my_page/profile_page/ProfilePage.tsx
+++ src/pages/my_page/profile_page/ProfilePage.tsx
@@ -23,8 +23,6 @@ const ProfilePage = () => {
     defaultValues: {
       nickname: "",
       email: "",
-      password: "",
-      newPassword: "",
     },
   });
 
--- src/pages/my_page/profile_page/components/password_update_modal/PasswordUpdateModal.tsx
+++ src/pages/my_page/profile_page/components/password_update_modal/PasswordUpdateModal.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useFormContext } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 import { toast } from "react-toastify";
 
 import useUpdatePassword from "apis/profile_apis/useUpdatePassword";
@@ -12,7 +12,13 @@ import { PASSWORD_PATTERN } from "constants/validations";
 import classes from "./PasswordUpdateModal.module.scss";
 
 const PasswordUpdateModal = ({ onClose }: { onClose: () => void }) => {
-  const methods = useFormContext();
+  const methods = useForm({
+    mode: "all",
+    defaultValues: {
+      password: "",
+      newPassword: "",
+    },
+  });
 
   const { mutate } = useUpdatePassword();
 
@@ -43,58 +49,60 @@ const PasswordUpdateModal = ({ onClose }: { onClose: () => void }) => {
   };
 
   return (
-    <motion.div
-      initial={{
-        opacity: 0,
-      }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.15 }}
-      className={classes.modal_backdrop}
-      onMouseDown={onClose}
-    >
+    <FormProvider {...methods}>
       <motion.div
         initial={{
           opacity: 0,
-          scale: 0.75,
         }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.75 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 0.15 }}
-        className={classes.modal_content}
-        onMouseDown={(e) => e.stopPropagation()}
+        className={classes.modal_backdrop}
+        onMouseDown={onClose}
       >
-        <div className={classes.modal_body}>
-          <label className={classes.input_wrap}>
-            <div>기존 비밀번호</div>
-            <BaseInput
-              type="password"
-              registerName="password"
-              placeholder="기존 비밀번호"
-              registerOption={{ required: "기존 비밀번호를 입력해 주세요." }}
-            />
-          </label>
-          <label className={classes.input_wrap}>
-            <div>새 비밀번호</div>
-            <BaseInput
-              type="password"
-              registerName="newPassword"
-              placeholder="새 비밀번호"
-              registerOption={{
-                required: "새 비밀번호를 입력해 주세요.",
-                pattern: {
-                  value: PASSWORD_PATTERN,
-                  message: "영문+숫자를 포함한 6자리 이상을 입력해 주세요.",
-                },
-              }}
-            />
-          </label>
-        </div>
-        <div className={classes.modal_footer}>
-          <TextButton text="변경하기" onClick={handleSubmit} />
-        </div>
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0.75,
+          }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.75 }}
+          transition={{ duration: 0.15 }}
+          className={classes.modal_content}
+          onMouseDown={(e) => e.stopPropagation()}
+        >
+          <div className={classes.modal_body}>
+            <label className={classes.input_wrap}>
+              <div>기존 비밀번호</div>
+              <BaseInput
+                type="password"
+                registerName="password"
+                placeholder="기존 비밀번호"
+                registerOption={{ required: "기존 비밀번호를 입력해 주세요." }}
+              />
+            </label>
+            <label className={classes.input_wrap}>
+              <div>새 비밀번호</div>
+              <BaseInput
+                type="password"
+                registerName="newPassword"
+                placeholder="새 비밀번호"
+                registerOption={{
+                  required: "새 비밀번호를 입력해 주세요.",
+                  pattern: {
+                    value: PASSWORD_PATTERN,
+                    message: "영문+숫자를 포함한 6자리 이상을 입력해 주세요.",
+                  },
+                }}
+              />
+            </label>
+          </div>
+          <div className={classes.modal_footer}>
+            <TextButton text="변경하기" onClick={handleSubmit} />
+          </div>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </FormProvider>
   );
 };
 
